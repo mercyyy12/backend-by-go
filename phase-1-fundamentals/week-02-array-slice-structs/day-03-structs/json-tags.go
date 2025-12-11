39c282c469fa06e158fa78afd8a2c9faf591813f
mercyyy12 <santoshkarki744@gmail.com>
2025-12-11 22:29:52 +0545

    json-tags

--- phase-1-fundamentals/week-02-array-slice-structs/day-03-structs/json-tags.go
+++ phase-1-fundamentals/week-02-array-slice-structs/day-03-structs/json-tags.go
@@ -6,25 +6,27 @@ import (
 )
 
 type Employee struct {
-	Name string `json:"name"`
+	Name string `json:"name"` //basic json tag
 	Age  int    `json:"age"`
-	Role string `json:"-"`
+	Role string `json:"-"` // ignoring a field
+
+	// embeded structure
 	Emp_address
 }
 
 type Emp_address struct {
 	Permanent_address string `json:"address"`
-	Phone             int    `json:"phone,omitempty"`
+	Phone             int    `json:"phone,omitempty"` // omit a field if empty
 }
 
 func Json_tags() {
 	e := Employee{"Mercyyy", 19, "sales", Emp_address{"kathmandu", 9841}}
 
-	// marshalling
+	// marshalling (go struct to json)
 	data, _ := json.MarshalIndent(e, "", "")
 	fmt.Println(string(data))
 
-	// unmarshalling
+	// unmarshalling (json to go struct)
 	var emp Employee
 	json.Unmarshal([]byte(data), &emp)
 	fmt.Println("Unmarshalled Struct:", emp)
