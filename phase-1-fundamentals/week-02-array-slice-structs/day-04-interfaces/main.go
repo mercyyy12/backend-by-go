2eae8b6304d9e1a78525d5a39a533653cd0a81f6
mercyyy12 <santoshkarki744@gmail.com>
2025-12-14 23:10:07 +0545

    interface, switch-type and type-assertion

--- phase-1-fundamentals/week-02-array-slice-structs/day-04-interfaces/main.go
+++ phase-1-fundamentals/week-02-array-slice-structs/day-04-interfaces/main.go
@@ -13,22 +13,60 @@ type sms struct {
 	number string
 }
 
+// Implement send() for email
 func (e email) send(msg string) string {
-	return fmt.Sprintf("Email sent to %s: %s", e.email, msg)
+	return fmt.Sprintf("\nEmail sent to %s: %s\n", e.email, msg)
 }
 
+// Implement send() for sms
 func (s sms) send(msg string) string {
-	return fmt.Sprintf("SMS sent to %s: %s", s.number, msg)
+	return fmt.Sprintf("\nSMS sent to %s: %s\n", s.number, msg)
 }
 
 func notify(n notifier, msg string) {
-	fmt.Println(n.send(msg))
+
+	// type switch
+	switch v := n.(type) {
+	case email:
+		fmt.Println("Handled as Email", v.send(msg))
+	case sms:
+		fmt.Println("Handled as SMS", v.send(msg))
+	default:
+		fmt.Println("Unknown notifier type")
+	}
+
 }
 
 func main() {
-	em := email{"mercyyy@gmail.com"}
-	notify(em, "santosh")
+	var em notifier = email{"mercyyy@gmail.com"}
+	notify(em, "How are you?")
 
 	sm := sms{"98545"}
-	notify(sm, "santosh89654")
+	notify(sm, "Yo! What's up?")
+
+	// empty interface (when data is unknown)
+	var x any
+	// or
+	// var x interface{}
+	x = 8
+	fmt.Println(x)
+	x = "Mercyyy"
+	fmt.Println(x)
+
+	// type assertion (similar to maps)
+	eml, ok := em.(email)
+	if ok {
+		fmt.Println(eml, ok)
+	} else {
+		fmt.Println(ok)
+	}
+
+	// Nil in interface
+	var ni interface{} = nil // Interface is nil
+	fmt.Println(ni == nil)
+
+	var e *email = nil // Interface holds a typed nil
+	var isnil notifier = e
+	fmt.Println(isnil == nil)
+
 }
