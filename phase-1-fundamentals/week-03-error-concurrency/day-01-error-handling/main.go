cde33922bcf6f8834e48b447d177a6e71cae5bae
mercyyy12 <santoshkarki744@gmail.com>
2025-12-26 23:33:58 +0545

    basic error handling

---
+++ phase-1-fundamentals/week-03-error-concurrency/day-01-error-handling/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Initializer sentinel error
+var ErrDivisionByZero = errors.New("division by zero")
+
+func main() {
+
+	// var err error = nil
+	// if err != nil {
+	// 	fmt.Println("There was an error:", err)
+	// } else {
+	// 	fmt.Println("No error!")
+	// }
+
+	a, b := 10, 0
+	c, err := divide(a, b)
+	if err != nil {
+
+		// panic stops prog immediately
+		// panic("something went terribly wrong")
+
+		fmt.Println("Error", err)
+		return
+	}
+	fmt.Println(c)
+
+}
+
+// Returning error from function
+func divide(a, b int) (int, error) {
+	if b == 0 {
+
+		// Creating simple error
+		// return 0, errors.New("cannot divide by zero!")
+
+		// Creating fromatted error
+		//return 0, fmt.Errorf("Can't divide %d by %d", a, b)
+
+		// Sentinel error
+		return 0, ErrDivisionByZero
+	}
+	return a / b, nil
+}
